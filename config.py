68ad469201fe758ff0d36111047a1852df677e88
nurigenc53 <144450151+nurigenc53@users.noreply.github.com>
2024-01-12 14:55:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class Config(object):
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1068141372").split())
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BACh59sABTZv45bUPzTuzxIH2kE7hHyotEmOQa8ZywOJ-ON3t0N_b6YSjpQKKGb9nFw9-9TzzOnFQ9K9jGmU0XdvVTcLc4Rof7TXTIG3sxBSB286xYTo9gvdzFRM6i7Bv1BgnOJF-yRRIOU7NK_TyL9-4jp8ie5rWagok8d_ntc40xZ2YJVE4wWjEwuilMTTvkXju7VKg-Coj0XzGrmnJ2oqd0L5vwmmUBBtXG7cYR1mO18mBOAhcHei5aw68VpOIApg5pb-4SsWprhfE4s7mNAn6pHYBVg9g96r4XnsjrfiTrQ50pvbmXDb7FQe3_Vhb6WFj5TbJunzLw6zJTXnWOJgyBKxtgAAAAA_qos8AA")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BACh59sATJCsj4XxYecCnFesRSMXQ00LsecVXnhnW-dLFd77dzWJ8xGt-f9H2bRimrw03tD4zdyRfbARksC0vnK7W8whMfoPYsGZhtGI62X3mldPIoB6BQvgkIt5tjSQBgwuU4ubMpYVC9IxVqGjIK85qTcvzayH3E8FLCJfUbH_iIf8kGsarFHwKiraqMtq5E426N-5myQNvmWtYjlcjhB2ujon7lHtyQwDgBKAzOu6icd8TqrjZcqLF8zbYYzzRlH590fIAYvjJuml91XoxAhBL9MeHBxvT149yUuIG63SK8b1QtdyRAgF1nBVRuljCF2iFzHQ7Pkb1g9lzFgosXM01SDYnwAAAAA_qos8AA")
 
 
 def LOGGER(name: str) -> logging.Logger:
